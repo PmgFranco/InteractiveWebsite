db10369e5d5cb656cf80464cf7da5f8e93497269
pmfranco <pmfranco@kpmg.com>
2016-07-15 17:17:34 +0100

    FInished lesson 2

--- zarcov/app.js
+++ zarcov/app.js
@@ -6,8 +6,15 @@ $(document).ready(function(){
 
       $('.js-like').on('click',function(event){
 
-      event.preventDefault(); #Prevents the Link behaviour to go to some other page
-      $(this).text('Liked!').addClass('is-liked');
+        event.preventDefault(); //Prevents the Link behaviour to go to some other page
+        $(this).text('Bazingaaa').closest('.news-item').addClass('is-liked');
 
-});
+      });
+
+      $('.js-add-link').on('click',function(event){
+
+        event.preventDefault(); //Prevents the Link behaviour to go to some other page
+        $('.js-form').toggleClass('is-visible');
+
+      });
 });
